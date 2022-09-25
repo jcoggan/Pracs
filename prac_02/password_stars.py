51399612362cc2c3d0e4e197fea5d2b6c0cef06a
jcoggan <jaiden.coggan@jcu.edu.au>
2022-09-25 20:53:19 +1000

    Add Password check program refactored

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,7 +1,20 @@
 MIN_LENGTH = 10
 
-password = input("Password: ")
-while len(password) < MIN_LENGTH:
-    print("Invalid password")
+def main():
+    password = get_password()
+    print_asterisks(password)
+
+
+def print_asterisks(password):
+    print("*" * len(password))
+
+
+def get_password():
     password = input("Password: ")
-print("*" * len(password))
+    while len(password) < MIN_LENGTH:
+        print("Invalid password")
+        password = input("Password: ")
+    return password
+
+
+main()
