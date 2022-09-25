134b7d9c4b530a8850df852c121fb79c989a28b2
jcoggan <jaiden.coggan@jcu.edu.au>
2022-09-25 20:54:40 +1000

    Add prac_01 programs to refract

---
+++ prac_02/electricity_bill.py
@@ -0,0 +1,20 @@
+"""
+Electricity Bill
+
+price per kWh in cents,
+daily use in kWh, and
+number of days in the billing period.
+
+pseudocode:
+get price_per_kWh
+get daily_usage
+get billing_period
+estimated_bill = (price_per_kWh * daily_usage * billing_period)
+display estimated_bill in $
+"""
+
+price_per_kWh = int(input())
+daily_usage =
+billing_period =
+estimated_bill = (price_per_kWh * daily_usage * billing_period)
+display estimated_bill in $
