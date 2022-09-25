3bb5bff65757daecc605efd8645be760e7bae7d7
jcoggan <jaiden.coggan@jcu.edu.au>
2022-09-25 21:37:59 +1000

    refracted score program

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,14 +2,27 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
-score = float(input("Enter score: "))
-while score > 100 or score < 0:
-    print("Invalid score")
+def main():
     score = float(input("Enter score: "))
-if score < 50:
-    print("Bad")
-elif score < 90:
-    print("Pass")
-else:
-    print("Excellent")
+    while score > 100 or score < 0:
+        print("Invalid score")
+        score = float(input("Enter score: "))
+    result = determine_result(score)
+    print(result)
+    score = random.randint(0, 100)
+    result = determine_result(score)
+    print(result)
+
+
+
+def determine_result(score):
+    if score < 50:
+        return "Bad"
+    elif score < 90:
+        return "Pass"
+    return "Excellent"
+
+
+main()
