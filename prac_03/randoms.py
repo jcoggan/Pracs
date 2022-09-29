c0b9a2d42b3ce60ca7a95ca6e14099c697908193
jcoggan <jaiden.coggan@jcu.edu.au>
2022-09-29 15:57:18 +1000

    Add randoms program

---
+++ prac_03/randoms.py
@@ -0,0 +1,21 @@
+"""
+What did you see on line 1?
+What was the smallest number you could have seen, what was the largest?
+
+Smallest number was 5 and the largest number was 20
+
+What did you see on line 2?
+What was the smallest number you could have seen, what was the largest?
+Could line 2 have produced a 4?
+
+Smallest number was 3 and the largest number was 9
+line 2 only gave numbers which are odd therefore is unable to produce a 4
+
+What did you see on line 3?
+What was the smallest number you could have seen, what was the largest?
+
+The Smallest number I could have seen would be 2.5 and the largest number would be 5.5
+"""
+import random
+
+print(random.randint(1, 100))
