0039ce4681c0c3f3873be7b0d6897a6329387048
jcoggan <jaiden.coggan@jcu.edu.au>
2022-10-06 16:17:17 +1000

    Add print subject details to subject reader program

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,12 +8,19 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    print_subject_details(data)
+
+
+def print_subject_details(data):
+    """prints subject details"""
+    for details in data:
+        print(f"{details[0]} is taught by {details[1]} and has {details[2]} students")
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    data = []
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -23,7 +30,9 @@ def get_data():
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
         print("----------")
+        data.append(parts)
     input_file.close()
+    return data
 
 
 main()
