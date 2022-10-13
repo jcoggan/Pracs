7547d6bed5b5c59a3872b08737740de02cea025b
jcoggan <jaiden.coggan@jcu.edu.au>
2022-10-13 16:03:14 +1000

    Revert "Add state name program"
    This reverts commit d5606681e5b4f28302f2e2b039dd4fa4b941ed39.

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,19 +4,15 @@ State names in a dictionary
 File needs reformatting
 """
 
-
-CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
-                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+# TODO: Reformat this file so the dictionary code follows PEP 8 convention
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT" : "Northern Territory", "WA" : "Western Australia",
+            "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ").upper()
+state_code = input("Enter short state: ")
 while state_code != "":
-    try:
-        print(f"{state_code:3} is {CODE_TO_NAME[state_code]}")
-    except KeyError:
+    if state_code in CODE_TO_NAME:
+        print(state_code, "is", CODE_TO_NAME[state_code])
+    else:
         print("Invalid short state")
-    state_code = input("Enter short state: ").upper()
-
-# Prints all the states and names neatly lined up
-for state_code in CODE_TO_NAME.keys():
-    print(f"{state_code:3} is {CODE_TO_NAME[state_code]}")
+    state_code = input("Enter short state: ")
