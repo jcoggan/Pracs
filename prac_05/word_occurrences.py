5f3b7071fb06d0f3f0e02e1b18c4a2f6d64bf1eb
jcoggan <jaiden.coggan@jcu.edu.au>
2022-10-18 10:20:18 +1000

    added dictionary converter to word occurrence program

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -3,3 +3,12 @@ Word Occurrences
 Estimate: 30 minutes
 Actual: ? minutes
 """
+
+words = {}
+text = input("Text:")
+for word in text.split(" "):
+    if word in words:
+        words[word] = words[word] + 1
+    else:
+        words[word] = 1
+print(words)
