83fc8ce6c991b99be2e7d3c084c5bff2bcb695d6
jcoggan <jaiden.coggan@jcu.edu.au>
2022-10-18 11:17:39 +1000

    added name extraction and broke into functions to emails program

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -4,15 +4,25 @@ Estimate: 20 minutes
 Actual: ? minutes
 """
 
-email_to_name = {}
-email = input("Email: ")
-while email != "":
-    name = email.split("@")[0]
-    isname = input(f"Is your name {name}? (Y/n)").lower()
-    if isname == "n" or isname == "no":
-        name = input("Name: ")
-    email_to_name[email] = name
-    email = input("Email: ")
 
-for email, name in email_to_name.items():
-    print(f"{name} ({email})")
+def main():
+    email_to_name = {}
+    email = input("Email: ")
+    while email != "":
+        name = extract_name(email)
+        isname = input(f"Is your name {name}? (Y/n)").lower()
+        if isname == "n" or isname == "no":
+            name = input("Name: ")
+        email_to_name[email] = name
+        email = input("Email: ")
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
+
+def extract_name(email):
+    username = email.split("@")[0]
+    name = " ".join(username.split(".")).title()
+    return name
+
+
+main()
