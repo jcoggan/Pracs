ed88dc9fe852b73efbdabcec8132f21718cc4cfb
jcoggan <jaiden.coggan@jcu.edu.au>
2022-10-18 10:33:59 +1000

    added formatting to word occurrence program

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,16 +1,19 @@
 """
 Word Occurrences
 Estimate: 30 minutes
-Actual: ? minutes
+Actual: 18 minutes
 """
 
 word_to_occurrences = {}
+longest_word_length = 0
 text = input("Text: ")
 for word in text.split(" "):
     if word in word_to_occurrences:
         word_to_occurrences[word] = word_to_occurrences[word] + 1
     else:
         word_to_occurrences[word] = 1
+    if len(word) > longest_word_length:
+        longest_word_length = len(word)
 
-for word in word_to_occurrences:
-    print(f"{word} : {word_to_occurrences[word]}")
+for word in sorted(word_to_occurrences):
+    print(f"{word:{longest_word_length}} : {word_to_occurrences[word]}")
