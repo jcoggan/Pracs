c93a16e08ccc873233d5c4d9ed4351845fd3e0a8
jcoggan <jaiden.coggan@jcu.edu.au>
2022-11-07 19:45:13 +1000

    finished guitar task

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -2,11 +2,26 @@ from guitar import Guitar
 
 FILENAME = "guitars.csv"
 guitars = []
-with open(FILENAME, "r", encoding="utf8") as in_file:
-    for line in in_file:
+with open(FILENAME, "r+", encoding="utf8") as guitar_file:
+    for line in guitar_file:
         guitar_parts = line.strip().split(",")
         guitar = Guitar(guitar_parts[0], int(guitar_parts[1]), float(guitar_parts[2]))
         guitars.append(guitar)
+    print("Current Guitars")
+    for guitar in sorted(guitars):
+        print(guitar)
 
-for guitar in sorted(guitars):
-    print(guitar)
+    print("New Guitars")
+    name = input("Name: ")
+    if name != "":
+        year = int(input("Year: "))
+        cost = int(input("Cost: "))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(f"{guitar} added")
+        name = input("Name: ")
+
+    guitar_file.seek(0)
+    for guitar in guitars:
+        print(f"{guitar.name}, {guitar.year}, {guitar.cost}", file=guitar_file)
+    guitar_file.truncate()
