fb133e37bf6ecc10acf0d0314235fe20d34a0590
jcoggan <jaiden.coggan@jcu.edu.au>
2022-11-07 19:28:44 +1000

    Added printing sorted list

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -1,5 +1,4 @@
-from prac_06.guitar import Guitar
-from operator import itemgetter
+from guitar import Guitar
 
 FILENAME = "guitars.csv"
 guitars = []
@@ -8,3 +7,6 @@ with open(FILENAME, "r", encoding="utf8") as in_file:
         guitar_parts = line.strip().split(",")
         guitar = Guitar(guitar_parts[0], int(guitar_parts[1]), float(guitar_parts[2]))
         guitars.append(guitar)
+
+for guitar in sorted(guitars):
+    print(guitar)
